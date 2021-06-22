15f1298834c7e62d0ac555008bd540cc87cc427a
endru-toptal <noviar.endru@toptal.com>
2021-06-22 16:51:24 +0700

    Add companyAuthToken to IterateInMemoryCache

--- iterate/src/main/java/com/iteratehq/iterate/data/local/IterateInMemoryCache.kt
+++ iterate/src/main/java/com/iteratehq/iterate/data/local/IterateInMemoryCache.kt
@@ -1,15 +1,26 @@
 package com.iteratehq.iterate.data.local
 
 internal interface IterateInMemoryCache {
+    fun getCompanyAuthToken(): String?
     fun getPreviewSurveyId(): String?
+    fun setCompanyAuthToken(companyAuthToken: String)
     fun setPreviewSurveyId(previewSurveyId: String)
 }
 
 internal object IterateInMemoryCacheImpl : IterateInMemoryCache {
+    private var companyAuthToken: String? = null
     private var previewSurveyId: String? = null
+
+    override fun getCompanyAuthToken(): String? {
+        return companyAuthToken
+    }
 
     override fun getPreviewSurveyId(): String? {
         return previewSurveyId
+    }
+
+    override fun setCompanyAuthToken(companyAuthToken: String) {
+        this.companyAuthToken = companyAuthToken
     }
 
     override fun setPreviewSurveyId(previewSurveyId: String) {
